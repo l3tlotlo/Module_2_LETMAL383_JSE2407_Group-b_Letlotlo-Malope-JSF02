880e5bbbf35a5077ae426cb48103d9d9185019b1
Letlotlo Malope <letlotlomal@gmail.com>
2024-07-31 19:07:36 +0200

    Created and populated ProductList.svelte componen

---
+++ src/components/ProductList.svelte
@@ -0,0 +1,23 @@
+<script>
+    import ProductCard from './ProductCard.svelte';
+    import { writable } from 'svelte/store';
+  
+    export let products = writable([]);
+    export let wishlist;
+    export let cart;
+  </script>
+  
+  <div class="grid">
+    {#each $products as product}
+      <ProductCard {product} {wishlist} {cart} />
+    {/each}
+  </div>
+  
+  <style>
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+      gap: 16px;
+    }
+  </style>
+  
